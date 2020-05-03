eed5cac3c6a6e0b3975d75f8fc5e440ae10b3f7a
Ruby <rubymaejmorante@gmail.com>
2020-05-03 14:10:02 +0800

    package.json and app.js

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+//const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -23,7 +26,7 @@ const userSchema = new mongoose.Schema({ //level up the construction of schema f
 });
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
+// userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]}); //mongoose encryption
 
 const User = new mongoose.model("User", userSchema);
 
@@ -41,17 +44,18 @@ app.get("/register", function(req, res){
 
 
 app.post("/register", function (req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    });
   });
 
 });
@@ -68,9 +72,9 @@ app.post("/login", function(req, res){
     if(err){
       console.log(err);
     }else{
-      if(foundUser.password === password){
-        res.render("secrets");
-      }
+      bcrypt.compare(password, foundUser.password, function(err, result) {
+      res.render("secrets");
+      });
     }
   });
 });
